c8619172b942eb9f603f18fcce34a57666029270
gurunaresh <gnareshblaze@gmail.com>
2023-10-27 11:52:34 +0530

    Add files via upload

---
+++ studentmanagement/src/main/java/com/gl/studentmanagement/service/StudentServiceImpl.java
@@ -0,0 +1,53 @@
+package com.gl.studentmanagement.service;
+
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.gl.studentmanagement.entity.Student;
+import com.gl.studentmanagement.repository.StudentRepository;
+
+@Repository
+public class StudentServiceImpl implements StudentService {
+
+	@Autowired
+	StudentRepository studentRepository;
+
+	@Transactional
+	public List<Student> findAll() {
+
+		List<Student> students = studentRepository.findAll();
+
+		return students;
+	}
+
+	@Transactional
+	public Student findById(int id) {
+
+		Student student = new Student();
+
+		// find record with Id from the database table
+		student = studentRepository.findById(id).get();
+
+		return student;
+	}
+
+	@Transactional
+	public void save(Student theStudent) {
+
+		studentRepository.save(theStudent);
+
+	}
+
+	@Transactional
+	public void deleteById(int id) {
+
+		studentRepository.deleteById(id);
+
+	}
+
+}
